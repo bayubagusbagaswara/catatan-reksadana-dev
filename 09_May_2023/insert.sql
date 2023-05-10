b7e07be5905f38f33a7dcb635222e840192a0352
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-05-10 14:09:40 +0700

    add sample get all risk question

---
+++ 09_May_2023/insert.sql
@@ -0,0 +1,32 @@
+INSERT INTO PARAMETER_MANAGEMENT_SERVICE.CONTENT_PARAMETER (
+        CREATED_BY,
+        CREATED_DATE,
+        DESCRIPTION,
+        MODIFIED_BY,
+        MODIFIED_DATE,
+        MODULE,
+        NAME,
+        VALUE,
+        "LANGUAGE",
+        "ORDERING",
+        LABEL1,
+        LABEL2,
+        IMAGE_URL,
+        ADDITIONAL_DATA
+    )
+VALUES(
+        'SYSTEM',
+        TIMESTAMP '2023-05-09 10:53:43.018000',
+        'Produk dan Layanan Pembukaan Reksadana',
+        NULL,
+        NULL,
+        'REKSADANA_PRODUCT_LAYANAN',
+        'INVEST_OPENING_REKSADANA',
+        '-',
+        'IDN',
+        1,
+        NULL,
+        NULL,
+        NULL,
+        NULL
+    );
