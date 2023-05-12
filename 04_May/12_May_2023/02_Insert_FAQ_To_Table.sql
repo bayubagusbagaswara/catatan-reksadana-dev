ac62e6152b5a341b1c8139311137e51a8230bad9
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-05-12 15:55:38 +0700

    add folder 12 May 2023

---
+++ 04_May/12_May_2023/02_Insert_FAQ_To_Table.sql
@@ -0,0 +1,34 @@
+INSERT INTO PARAMETER_MANAGEMENT_SERVICE.FAQ (
+        ID_FAQ_CATEGORY,
+        QUESTION_IDN,
+        QUESTION_ENG,
+        ANSWER_IDN,
+        ANSWER_ENG,
+        KEYWORDS,
+        LIKES,
+        DISLIKES,
+        SEEN_BY_USER,
+        CREATED_BY,
+        CREATED_DATE,
+        MODIFIED_BY,
+        MODIFIED_DATE,
+        IS_DELETED,
+        STATUS
+    )
+VALUES(
+        22,
+        'Apa itu reksa dana?',
+        'Apa itu reksa dana',
+        'Adalah wadah yang menggabungkan modal dari banyak investor, berinvestasi pada beragam instrumen serta dikelola secara profesional oleh perusahaan pengelola aset (Manajer Investasi)',
+        'Adalah wadah yang menggabungkan modal dari banyak investor, berinvestasi pada beragam instrumen serta dikelola secara profesional oleh perusahaan pengelola aset (Manajer Investasi)',
+        'Reksa dana',
+        NULL,
+        NULL,
+        NULL,
+        'SYSTEM',
+        TIMESTAMP '2022-06-13 17:43:36.896000',
+        NULL,
+        NULL,
+        0,
+        1
+    );
