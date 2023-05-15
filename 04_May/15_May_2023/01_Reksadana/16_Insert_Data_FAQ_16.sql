30baca4f17df525c56a614f48229ba83025acd6e
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-05-15 10:19:59 +0700

    insert data faq 16

---
+++ 04_May/15_May_2023/01_Reksadana/16_Insert_Data_FAQ_16.sql
@@ -0,0 +1,34 @@
+INSERT INTO PARAMETER_MANAGEMENT_SERVICE.FAQ (
+        ID_FAQ_CATEGORY,
+        QUESTION_IDN,
+        QUESTION_ENG,
+        ANSWER_IDN,
+        ANSWER_ENG,
+        KEYWORDS,
+        LIKES,
+        DISLIKES,
+        SEEN_BY_USER,
+        CREATED_BY,
+        CREATED_DATE,
+        MODIFIED_BY,
+        MODIFIED_DATE,
+        IS_DELETED,
+        STATUS
+    )
+VALUES(
+        801,
+        'Apakah ada jam operasional transaksi di mobile apps?',
+        'Apakah ada jam operasional transaksi di mobile apps?',
+        'Tidak ada, seluruh fitur dapat diakses dan ditransaksikan selama 24 jam/7 hari',
+        'Tidak ada, seluruh fitur dapat diakses dan ditransaksikan selama 24 jam/7 hari',
+        'Reksa Dana',
+        0,
+        0,
+        0,
+        'ADMINPORTAL',
+        TIMESTAMP '2023-05-15 04:19:23.840',
+        NULL,
+        NULL,
+        0,
+        0
+    );
