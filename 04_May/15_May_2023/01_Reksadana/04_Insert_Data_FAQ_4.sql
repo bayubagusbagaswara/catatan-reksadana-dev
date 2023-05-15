41dc2b2b35fb6441ba5888b79289079c9a7fb118
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-05-15 09:54:31 +0700

    insert data faq 4

---
+++ 04_May/15_May_2023/01_Reksadana/04_Insert_Data_FAQ_4.sql
@@ -0,0 +1,34 @@
+INSERT INTO PARAMETER_MANAGEMENT_SERVICE.FAQ (
+        ID_FAQ_CATEGORY,
+        QUESTION_IDN,
+        QUESTION_ENG,
+        ANSWER_IDN,
+        ANSWER_ENG,
+        KEYWORDS,
+        LIKES,
+        DISLIKES,
+        SEEN_BY_USER,
+        CREATED_BY,
+        CREATED_DATE,
+        MODIFIED_BY,
+        MODIFIED_DATE,
+        IS_DELETED,
+        STATUS
+    )
+VALUES(
+        801,
+        'Apa Perbedaan Reksadana Syariah dan Konvensional?',
+        'Apa Perbedaan Reksadana Syariah dan Konvensional?',
+        'Syariah: Dikelola sesuai prinsip syariah. Investasi hanya pada Efek Syariah. Terdapat mekanisme pembersihan harta Non-Halal (Cleansing). Wajib dikelola oleh profesional yang mengerti kegiatan yang dilarang berdasarkan prinsip syariah. Konvensional: Dikelola tanpa memperhatikan prinsip syariah. Investasi pada Seluruh Efek. Tidak menggunakan konsep pembersihan harta. Tidak perlu profesional yang mengerti kegiatan yang dilarang berdasarkan prinsip syariah.',
+        'Syariah: Dikelola sesuai prinsip syariah. Investasi hanya pada Efek Syariah. Terdapat mekanisme pembersihan harta Non-Halal (Cleansing). Wajib dikelola oleh profesional yang mengerti kegiatan yang dilarang berdasarkan prinsip syariah. Konvensional: Dikelola tanpa memperhatikan prinsip syariah. Investasi pada Seluruh Efek. Tidak menggunakan konsep pembersihan harta. Tidak perlu profesional yang mengerti kegiatan yang dilarang berdasarkan prinsip syariah.',
+        'Reksa Dana',
+        0,
+        0,
+        0,
+        'ADMINPORTAL',
+        TIMESTAMP '2023-05-15 03:53:18.943',
+        NULL,
+        NULL,
+        0,
+        0
+    );
