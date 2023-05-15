4269d64916c692972f3b4caa183032cfacd1450b
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-05-15 10:42:28 +0700

    insert data faq 1

---
+++ 04_May/15_May_2023/03_Beli_Reksadana/01_Insert_Data_FAQ_1.sql
@@ -0,0 +1,34 @@
+INSERT INTO PARAMETER_MANAGEMENT_SERVICE.FAQ (
+        ID_FAQ_CATEGORY,
+        QUESTION_IDN,
+        QUESTION_ENG,
+        ANSWER_IDN,
+        ANSWER_ENG,
+        KEYWORDS,
+        LIKES,
+        DISLIKES,
+        SEEN_BY_USER,
+        CREATED_BY,
+        CREATED_DATE,
+        MODIFIED_BY,
+        MODIFIED_DATE,
+        IS_DELETED,
+        STATUS
+    )
+VALUES(
+        822,
+        'Bagaimana cara melakukan pengajuan pembelian reksa dana?',
+        'Bagaimana cara melakukan pengajuan pembelian reksa dana?',
+        'Nasabah dapat membeli melalui fitur beli dan melalui proses explorasi produk Pengajuan Pembelian Reksa Dana Online',
+        'Nasabah dapat membeli melalui fitur beli dan melalui proses explorasi produk Pengajuan Pembelian Reksa Dana Online',
+        'Reksa Dana, Beli',
+        0,
+        0,
+        0,
+        'ADMINPORTAL',
+        TIMESTAMP '2023-05-15 04:41:26.943',
+        NULL,
+        NULL,
+        0,
+        0
+    );
