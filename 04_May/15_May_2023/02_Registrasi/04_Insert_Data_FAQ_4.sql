25c44f7fc791b286fb394b142ea012cc07322657
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-05-15 10:31:29 +0700

    insert data faq 4

---
+++ 04_May/15_May_2023/02_Registrasi/04_Insert_Data_FAQ_4.sql
@@ -0,0 +1,34 @@
+INSERT INTO PARAMETER_MANAGEMENT_SERVICE.FAQ (
+        ID_FAQ_CATEGORY,
+        QUESTION_IDN,
+        QUESTION_ENG,
+        ANSWER_IDN,
+        ANSWER_ENG,
+        KEYWORDS,
+        LIKES,
+        DISLIKES,
+        SEEN_BY_USER,
+        CREATED_BY,
+        CREATED_DATE,
+        MODIFIED_BY,
+        MODIFIED_DATE,
+        IS_DELETED,
+        STATUS
+    )
+VALUES(
+        821,
+        'Bagaimana penginformasian apabila SID dan IFUA untuk transaksi reksa dana sudah terbentuk?',
+        'Bagaimana penginformasian apabila SID dan IFUA untuk transaksi reksa dana sudah terbentuk?',
+        'Informasi bahwa SID dan IFUA sudah terbentuk akan disampaikan melalui email nasabah yang telah didaftarkan pada proses registrasi',
+        'Informasi bahwa SID dan IFUA sudah terbentuk akan disampaikan melalui email nasabah yang telah didaftarkan pada proses registrasi',
+        'Reksa Dana, Registrasi',
+        0,
+        0,
+        0,
+        'ADMINPORTAL',
+        TIMESTAMP '2023-05-15 04:30:49.490',
+        NULL,
+        NULL,
+        0,
+        0
+    );
