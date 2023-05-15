2d8974ea84564d9b7b5c770fec3df607f248e277
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-05-15 10:53:42 +0700

    insert data faq 1

---
+++ 04_May/15_May_2023/04_Jual_Reksadana/01_Insert_Data_FAQ_1.sql
@@ -0,0 +1,34 @@
+INSERT INTO PARAMETER_MANAGEMENT_SERVICE.FAQ (
+        ID_FAQ_CATEGORY,
+        QUESTION_IDN,
+        QUESTION_ENG,
+        ANSWER_IDN,
+        ANSWER_ENG,
+        KEYWORDS,
+        LIKES,
+        DISLIKES,
+        SEEN_BY_USER,
+        CREATED_BY,
+        CREATED_DATE,
+        MODIFIED_BY,
+        MODIFIED_DATE,
+        IS_DELETED,
+        STATUS
+    )
+VALUES(
+        823,
+        'Bagaimana cara melakukan penjualan produk reksa dana?',
+        'Bagaimana cara melakukan penjualan produk reksa dana?',
+        'Nasabah dapat membeli melalui fitur beli dan melalui proses explorasi produk',
+        'Nasabah dapat membeli melalui fitur beli dan melalui proses explorasi produk',
+        'Reksa Dana, Jual',
+        0,
+        0,
+        0,
+        'ADMINPORTAL',
+        TIMESTAMP '2023-05-15 04:52:04.727',
+        NULL,
+        NULL,
+        0,
+        0
+    );
