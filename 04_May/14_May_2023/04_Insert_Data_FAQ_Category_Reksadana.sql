b8dd7a8ea9880cac1d4e6bcadc6388edc70eb866
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-05-15 09:02:43 +0700

    insert data FAQ category reksadana

---
+++ 04_May/14_May_2023/04_Insert_Data_FAQ_Category_Reksadana.sql
@@ -0,0 +1,30 @@
+INSERT INTO PARAMETER_MANAGEMENT_SERVICE.FAQ_CATEGORY (
+        CREATED_BY,
+        CREATED_DATE,
+        MODIFIED_BY,
+        MODIFIED_DATE,
+        NAME_IDN,
+        NAME_ENG,
+        IMAGE_URL,
+        PRIORITY,
+        IS_DELETED,
+        STATUS,
+        CATEGORY_CODE,
+        MODULE
+    )
+VALUES(
+        'ADMIN',
+        TIMESTAMP '2023-05-14 14:41:24.520',
+        NULL,
+        NULL,
+        'Reksadana',
+        'Reksadana',
+        'http://mf8.myinfosys.net:7005/bsi/assets/FaqCategory/FaqCategoryQCV6ThuMay0410:50:22WIB2023.png',
+        1,
+        0,
+        '1',
+        NULL,
+        NULL,
+        'REKSADANA',
+        'REKSADANA'
+    );
