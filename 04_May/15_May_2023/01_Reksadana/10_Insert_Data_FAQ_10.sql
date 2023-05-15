b876d461f01c1aab04289ccb6aaea2d60fc70679
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-05-15 10:08:36 +0700

    insert data faq 10

---
+++ 04_May/15_May_2023/01_Reksadana/10_Insert_Data_FAQ_10.sql
@@ -0,0 +1,34 @@
+INSERT INTO PARAMETER_MANAGEMENT_SERVICE.FAQ (
+        ID_FAQ_CATEGORY,
+        QUESTION_IDN,
+        QUESTION_ENG,
+        ANSWER_IDN,
+        ANSWER_ENG,
+        KEYWORDS,
+        LIKES,
+        DISLIKES,
+        SEEN_BY_USER,
+        CREATED_BY,
+        CREATED_DATE,
+        MODIFIED_BY,
+        MODIFIED_DATE,
+        IS_DELETED,
+        STATUS
+    )
+VALUES(
+        801,
+        'Apakah produk reksa dana dan perusahaan investasi dijamin regulator?',
+        'Apakah produk reksa dana dan perusahaan investasi dijamin regulator?',
+        'Dana investasi dikelola oleh perusahaan yang memiliki izin dan diawasi oleh Otoritas Jasa Keuangan (OJK).',
+        'Dana investasi dikelola oleh perusahaan yang memiliki izin dan diawasi oleh Otoritas Jasa Keuangan (OJK).',
+        'Reksa Dana',
+        0,
+        0,
+        0,
+        'ADMINPORTAL',
+        TIMESTAMP '2023-05-15 04:07:21.653',
+        NULL,
+        NULL,
+        0,
+        0
+    );
