318e6a84695bdbadd6c802bde2164cb17f6025bc
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-05-15 10:35:44 +0700

    insert data faq 6

---
+++ 04_May/15_May_2023/02_Registrasi/06_Insert_Data_FAQ_6.sql
@@ -0,0 +1,34 @@
+INSERT INTO PARAMETER_MANAGEMENT_SERVICE.FAQ (
+        ID_FAQ_CATEGORY,
+        QUESTION_IDN,
+        QUESTION_ENG,
+        ANSWER_IDN,
+        ANSWER_ENG,
+        KEYWORDS,
+        LIKES,
+        DISLIKES,
+        SEEN_BY_USER,
+        CREATED_BY,
+        CREATED_DATE,
+        MODIFIED_BY,
+        MODIFIED_DATE,
+        IS_DELETED,
+        STATUS
+    )
+VALUES(
+        821,
+        'Apa tujuan melakukan pengisian profil risiko?',
+        'Apa tujuan melakukan pengisian profil risiko?',
+        'Nasabah dapat mengetahui karakter untuk berinvestasi investasi yaitu termasuk yang, konservatif, moderat, growth atau aggresif.',
+        'Nasabah dapat mengetahui karakter untuk berinvestasi investasi yaitu termasuk yang, konservatif, moderat, growth atau aggresif.',
+        'Reksa Dana, Registrasi',
+        0,
+        0,
+        0,
+        'ADMINPORTAL',
+        TIMESTAMP '2023-05-15 04:34:54.753',
+        NULL,
+        NULL,
+        0,
+        0
+    );
