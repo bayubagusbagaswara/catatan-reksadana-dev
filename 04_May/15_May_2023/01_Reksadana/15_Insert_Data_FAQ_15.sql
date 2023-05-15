b76a9b53151b14bc3090d2eebbfa00b8c07f5b3e
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-05-15 10:18:35 +0700

    insert data faq 15

---
+++ 04_May/15_May_2023/01_Reksadana/15_Insert_Data_FAQ_15.sql
@@ -0,0 +1,34 @@
+INSERT INTO PARAMETER_MANAGEMENT_SERVICE.FAQ (
+        ID_FAQ_CATEGORY,
+        QUESTION_IDN,
+        QUESTION_ENG,
+        ANSWER_IDN,
+        ANSWER_ENG,
+        KEYWORDS,
+        LIKES,
+        DISLIKES,
+        SEEN_BY_USER,
+        CREATED_BY,
+        CREATED_DATE,
+        MODIFIED_BY,
+        MODIFIED_DATE,
+        IS_DELETED,
+        STATUS
+    )
+VALUES(
+        801,
+        'Fitur apa aja yang tersedia pada menu reksa dana saat ini?',
+        'Fitur apa aja yang tersedia pada menu reksa dana saat ini?',
+        'Registrasi, Pembelian, Penjualan, dan Portfolio. Untuk fitur History, Switching dan Beli Rutin akan dirilis pada fase berikutnya',
+        'Registrasi, Pembelian, Penjualan, dan Portfolio. Untuk fitur History, Switching dan Beli Rutin akan dirilis pada fase berikutnya',
+        'Reksa Dana',
+        0,
+        0,
+        0,
+        'ADMINPORTAL',
+        TIMESTAMP '2023-05-15 04:18:01.930',
+        NULL,
+        NULL,
+        0,
+        0
+    );
