f1a64e5c5b395a3f98e8348a23c7e6ac0e4b4a4a
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-05-15 10:30:07 +0700

    insert data faq 3

---
+++ 04_May/15_May_2023/02_Registrasi/03_Insert_Data_FAQ_3.sql
@@ -0,0 +1,34 @@
+INSERT INTO PARAMETER_MANAGEMENT_SERVICE.FAQ (
+        ID_FAQ_CATEGORY,
+        QUESTION_IDN,
+        QUESTION_ENG,
+        ANSWER_IDN,
+        ANSWER_ENG,
+        KEYWORDS,
+        LIKES,
+        DISLIKES,
+        SEEN_BY_USER,
+        CREATED_BY,
+        CREATED_DATE,
+        MODIFIED_BY,
+        MODIFIED_DATE,
+        IS_DELETED,
+        STATUS
+    )
+VALUES(
+        821,
+        'Berapa lama waktu yang diperlukan untuk pembentukan SID dan IFUA?',
+        'Berapa lama waktu yang diperlukan untuk pembentukan SID dan IFUA?',
+        'Lama waktu (SLA) pembuatan SID dan IFUA maksimal 2 Hari Bursa (Hari Kerja KSEI)',
+        'Lama waktu (SLA) pembuatan SID dan IFUA maksimal 2 Hari Bursa (Hari Kerja KSEI)',
+        'Reksa Dana, Registrasi',
+        0,
+        0,
+        0,
+        'ADMINPORTAL',
+        TIMESTAMP '2023-05-15 04:29:32.740',
+        NULL,
+        NULL,
+        0,
+        0
+    );
