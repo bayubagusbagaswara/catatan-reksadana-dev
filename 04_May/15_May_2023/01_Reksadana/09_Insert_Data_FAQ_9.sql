f6dcbf36e7ac9e4b4b913e52b070be3873034a06
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-05-15 10:06:56 +0700

    insert data faq 9

---
+++ 04_May/15_May_2023/01_Reksadana/09_Insert_Data_FAQ_9.sql
@@ -0,0 +1,34 @@
+INSERT INTO PARAMETER_MANAGEMENT_SERVICE.FAQ (
+        ID_FAQ_CATEGORY,
+        QUESTION_IDN,
+        QUESTION_ENG,
+        ANSWER_IDN,
+        ANSWER_ENG,
+        KEYWORDS,
+        LIKES,
+        DISLIKES,
+        SEEN_BY_USER,
+        CREATED_BY,
+        CREATED_DATE,
+        MODIFIED_BY,
+        MODIFIED_DATE,
+        IS_DELETED,
+        STATUS
+    )
+VALUES(
+        801,
+        'Apakah nasabah harus memilih saham dan obligasi sendiri?',
+        'Apakah nasabah harus memilih saham dan obligasi sendiri?',
+        'Tidak perlu. Manajer Investasi akan melakukan pemilihan dan penempatan produk saham dan obligasi tersebut. Nasabah cukup memilih ingin fitur reksa dana saham, Pendapatan Tetap, Campuran atau Pasar Uang.',
+        'Tidak perlu. Manajer Investasi akan melakukan pemilihan dan penempatan produk saham dan obligasi tersebut. Nasabah cukup memilih ingin fitur reksa dana saham, Pendapatan Tetap, Campuran atau Pasar Uang.',
+        'Reksa Dana',
+        0,
+        0,
+        0,
+        'ADMINPORTAL',
+        TIMESTAMP '2023-05-15 04:06:14.990',
+        NULL,
+        NULL,
+        0,
+        0
+    );
