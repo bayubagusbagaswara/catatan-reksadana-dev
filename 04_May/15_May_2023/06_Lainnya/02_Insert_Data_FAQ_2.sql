64808a4cb7cf60386bba307d618da42ec2e20c66
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-05-15 11:05:29 +0700

    insert data faq 2

---
+++ 04_May/15_May_2023/06_Lainnya/02_Insert_Data_FAQ_2.sql
@@ -0,0 +1,34 @@
+INSERT INTO PARAMETER_MANAGEMENT_SERVICE.FAQ (
+        ID_FAQ_CATEGORY,
+        QUESTION_IDN,
+        QUESTION_ENG,
+        ANSWER_IDN,
+        ANSWER_ENG,
+        KEYWORDS,
+        LIKES,
+        DISLIKES,
+        SEEN_BY_USER,
+        CREATED_BY,
+        CREATED_DATE,
+        MODIFIED_BY,
+        MODIFIED_DATE,
+        IS_DELETED,
+        STATUS
+    )
+VALUES(
+        825,
+        'Bagaimana jika profil risiko nasabah expired?',
+        'Bagaimana jika profil risiko nasabah expired?',
+        'Nasabah akan mendapatkan pop up transaksi apabila profil risiko expired dan nasabah diarahkan melakukan pengisian profil risiko',
+        'Nasabah akan mendapatkan pop up transaksi apabila profil risiko expired dan nasabah diarahkan melakukan pengisian profil risiko',
+        'Reksa Dana, Lainnya',
+        0,
+        0,
+        0,
+        'ADMINPORTAL',
+        TIMESTAMP '2023-05-15 05:04:33.707',
+        NULL,
+        NULL,
+        0,
+        0
+    );
