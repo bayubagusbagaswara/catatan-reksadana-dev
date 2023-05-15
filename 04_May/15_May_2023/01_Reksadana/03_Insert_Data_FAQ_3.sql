609ba9422bc63e3178dbfc2e29ad963735d3876f
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-05-15 09:51:31 +0700

    insert data faq 3

---
+++ 04_May/15_May_2023/01_Reksadana/03_Insert_Data_FAQ_3.sql
@@ -0,0 +1,34 @@
+INSERT INTO PARAMETER_MANAGEMENT_SERVICE.FAQ (
+        ID_FAQ_CATEGORY,
+        QUESTION_IDN,
+        QUESTION_ENG,
+        ANSWER_IDN,
+        ANSWER_ENG,
+        KEYWORDS,
+        LIKES,
+        DISLIKES,
+        SEEN_BY_USER,
+        CREATED_BY,
+        CREATED_DATE,
+        MODIFIED_BY,
+        MODIFIED_DATE,
+        IS_DELETED,
+        STATUS
+    )
+VALUES(
+        801,
+        'Bagaimana Mekanisme Investasi Produk Reksa Dana',
+        'Bagaimana Mekanisme Investasi Produk Reksa Dana',
+        'Reksa dana merupakan wadah dengan pola pengelolaan dana/modal dari sekumpulan investor untuk berinvestasi dalam instrumen-instrumen investasi yang tersedia di Pasar dengan cara membeli unit penyertaan Reksa Dana. Dana ini kemudian dikelola oleh Manajer Investasi (MI) ke dalam portofolio  investasi, baik berupa saham, obligasi/sukuk dan pasar uang ataupun efek lainnya',
+        'Reksa dana merupakan wadah dengan pola pengelolaan dana/modal dari sekumpulan investor untuk berinvestasi dalam instrumen-instrumen investasi yang tersedia di Pasar dengan cara membeli unit penyertaan Reksa Dana. Dana ini kemudian dikelola oleh Manajer Investasi (MI) ke dalam portofolio  investasi, baik berupa saham, obligasi/sukuk dan pasar uang ataupun efek lainnya',
+        'Reksa Dana',
+        0,
+        0,
+        0,
+        'ADMINPORTAL',
+        TIMESTAMP '2023-05-15 03:48:39.570',
+        NULL,
+        NULL,
+        0,
+        0
+    );
