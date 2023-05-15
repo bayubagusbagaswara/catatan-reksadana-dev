6fae50597d3120052270a03f180fbf24dc28a5d3
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-05-15 10:39:14 +0700

    insert data faq 8

---
+++ 04_May/15_May_2023/02_Registrasi/08_Insert_Data_FAQ_8.sql
@@ -0,0 +1,34 @@
+INSERT INTO PARAMETER_MANAGEMENT_SERVICE.FAQ (
+        ID_FAQ_CATEGORY,
+        QUESTION_IDN,
+        QUESTION_ENG,
+        ANSWER_IDN,
+        ANSWER_ENG,
+        KEYWORDS,
+        LIKES,
+        DISLIKES,
+        SEEN_BY_USER,
+        CREATED_BY,
+        CREATED_DATE,
+        MODIFIED_BY,
+        MODIFIED_DATE,
+        IS_DELETED,
+        STATUS
+    )
+VALUES(
+        821,
+        'Apakah data cabang yang dipilih ketika registrasi bersifat wajib diisi?',
+        'Apakah data cabang yang dipilih ketika registrasi bersifat wajib diisi?',
+        'Iya, diisi menyesuaikan cabang terdekat dari lokasi nasabah. Cabang disini tidak akan berpengaruh pada transaksi reksa dana nasabah',
+        'Iya, diisi menyesuaikan cabang terdekat dari lokasi nasabah. Cabang disini tidak akan berpengaruh pada transaksi reksa dana nasabah',
+        'Reksa Dana, Registrasi',
+        0,
+        0,
+        0,
+        'ADMINPORTAL',
+        TIMESTAMP '2023-05-15 04:37:40.257',
+        NULL,
+        NULL,
+        0,
+        0
+    );
