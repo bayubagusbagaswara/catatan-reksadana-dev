fd7315f6cf93dd6c9baa864b7da945f401b1ddb5
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-05-15 11:00:50 +0700

    insert data faq 1

---
+++ 04_May/15_May_2023/05_Portfolio/01_Insert_Data_FAQ_1.sql
@@ -0,0 +1,34 @@
+INSERT INTO PARAMETER_MANAGEMENT_SERVICE.FAQ (
+        ID_FAQ_CATEGORY,
+        QUESTION_IDN,
+        QUESTION_ENG,
+        ANSWER_IDN,
+        ANSWER_ENG,
+        KEYWORDS,
+        LIKES,
+        DISLIKES,
+        SEEN_BY_USER,
+        CREATED_BY,
+        CREATED_DATE,
+        MODIFIED_BY,
+        MODIFIED_DATE,
+        IS_DELETED,
+        STATUS
+    )
+VALUES(
+        824,
+        'Bagaimana caranya jika nasabah ingin melihat portofolio Reksa Dana yang mereka miliki?',
+        'Bagaimana caranya jika nasabah ingin melihat portofolio Reksa Dana yang mereka miliki?',
+        'Nasabah dapat masuk melalui menu Portofolio, lalu total portofolio nasabah beserta detail produk muncul pada halaman tersebut',
+        'Nasabah dapat masuk melalui menu Portofolio, lalu total portofolio nasabah beserta detail produk muncul pada halaman tersebut',
+        'Reksa Dana, Portfolio',
+        0,
+        0,
+        0,
+        'ADMINPORTAL',
+        TIMESTAMP '2023-05-15 05:00:04.757',
+        NULL,
+        NULL,
+        0,
+        0
+    );
