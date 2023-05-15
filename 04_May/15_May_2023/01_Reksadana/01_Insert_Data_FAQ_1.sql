c45eca43f78846e139c6488f3eb4c47c4ae51247
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-05-15 09:38:26 +0700

    add insert data FAQ 1

---
+++ 04_May/15_May_2023/01_Reksadana/01_Insert_Data_FAQ_1.sql
@@ -0,0 +1,34 @@
+INSERT INTO PARAMETER_MANAGEMENT_SERVICE.FAQ (
+        ID_FAQ_CATEGORY,
+        QUESTION_IDN,
+        QUESTION_ENG,
+        ANSWER_IDN,
+        ANSWER_ENG,
+        KEYWORDS,
+        LIKES,
+        DISLIKES,
+        SEEN_BY_USER,
+        CREATED_BY,
+        CREATED_DATE,
+        MODIFIED_BY,
+        MODIFIED_DATE,
+        IS_DELETED,
+        STATUS
+    )
+VALUES(
+        801,
+        'Apa itu Reksa Dana?',
+        'Apa itu Reksa Dana?',
+        'dalah wadah yang menggabungkan modal dari banyak investor, berinvestasi pada beragam instrumen, serta dikelola secara profesional oleh perusahaan pengelola aset (Manajer Investasi)',
+        'dalah wadah yang menggabungkan modal dari banyak investor, berinvestasi pada beragam instrumen, serta dikelola secara profesional oleh perusahaan pengelola aset (Manajer Investasi)',
+        'Reksa Dana',
+        0,
+        0,
+        0,
+        'ADMINPORTAL',
+        TIMESTAMP '2023-05-15 03:34:21.860',
+        NULL,
+        NULL,
+        0,
+        0
+    );
