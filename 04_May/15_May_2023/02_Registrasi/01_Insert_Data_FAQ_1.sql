bb9ea13425d9c78fb273895c22e6f5587c762e82
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-05-15 10:27:11 +0700

    insert data faq 1

---
+++ 04_May/15_May_2023/02_Registrasi/01_Insert_Data_FAQ_1.sql
@@ -0,0 +1,34 @@
+INSERT INTO PARAMETER_MANAGEMENT_SERVICE.FAQ (
+        ID_FAQ_CATEGORY,
+        QUESTION_IDN,
+        QUESTION_ENG,
+        ANSWER_IDN,
+        ANSWER_ENG,
+        KEYWORDS,
+        LIKES,
+        DISLIKES,
+        SEEN_BY_USER,
+        CREATED_BY,
+        CREATED_DATE,
+        MODIFIED_BY,
+        MODIFIED_DATE,
+        IS_DELETED,
+        STATUS
+    )
+VALUES(
+        821,
+        'Apa saja yang diperlukan pada proses registrasi Reksa Dana?',
+        'Apa saja yang diperlukan pada proses registrasi Reksa Dana?',
+        'Nasabah diarahkan membaca informasi produk, melakukan pengisian profil risiko, upload dokumen KTP, pengisian data diri berupa email, pemilihan cabang dan persetujuan syarat dan ketentuan',
+        'Nasabah diarahkan membaca informasi produk, melakukan pengisian profil risiko, upload dokumen KTP, pengisian data diri berupa email, pemilihan cabang dan persetujuan syarat dan ketentuan',
+        'Reksa Dana, Registrasi',
+        0,
+        0,
+        0,
+        'ADMINPORTAL',
+        TIMESTAMP '2023-05-15 04:22:36.160',
+        NULL,
+        NULL,
+        0,
+        0
+    );
