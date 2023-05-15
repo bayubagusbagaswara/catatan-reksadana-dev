281a744e53a9efd186620e2120194a49f23b41ba
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-05-15 10:55:23 +0700

    insert data faq 2

---
+++ 04_May/15_May_2023/04_Jual_Reksadana/02_Insert_Data_FAQ_2.sql
@@ -0,0 +1,34 @@
+INSERT INTO PARAMETER_MANAGEMENT_SERVICE.FAQ (
+        ID_FAQ_CATEGORY,
+        QUESTION_IDN,
+        QUESTION_ENG,
+        ANSWER_IDN,
+        ANSWER_ENG,
+        KEYWORDS,
+        LIKES,
+        DISLIKES,
+        SEEN_BY_USER,
+        CREATED_BY,
+        CREATED_DATE,
+        MODIFIED_BY,
+        MODIFIED_DATE,
+        IS_DELETED,
+        STATUS
+    )
+VALUES(
+        823,
+        'Apakah terdapat pembatasan transaksi pengajuan penjualan reksa dana?',
+        'Apakah terdapat pembatasan transaksi pengajuan penjualan reksa dana?',
+        'Nasabah dapat melakukan pengajuan penjualan reksa dana melalui aplikasi kapan saja. Hanya saja apabila transaksi nasabah lebih dari jam cut off yaitu pukul 12.30 proses transaksi dan NAV akan mengikuti hari bursa berikutnya',
+        'Nasabah dapat melakukan pengajuan penjualan reksa dana melalui aplikasi kapan saja. Hanya saja apabila transaksi nasabah lebih dari jam cut off yaitu pukul 12.30 proses transaksi dan NAV akan mengikuti hari bursa berikutnya',
+        'Reksa Dana, Jual',
+        0,
+        0,
+        0,
+        'ADMINPORTAL',
+        TIMESTAMP '2023-05-15 04:54:28.090',
+        NULL,
+        NULL,
+        0,
+        0
+    );
