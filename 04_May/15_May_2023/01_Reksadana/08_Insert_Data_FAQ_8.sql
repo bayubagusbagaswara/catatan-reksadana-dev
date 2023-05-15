e6d2a850e8d4d70906324f97a7fa632b487b4ce3
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-05-15 10:05:26 +0700

    insert data faq 8

---
+++ 04_May/15_May_2023/01_Reksadana/08_Insert_Data_FAQ_8.sql
@@ -0,0 +1,34 @@
+INSERT INTO PARAMETER_MANAGEMENT_SERVICE.FAQ (
+        ID_FAQ_CATEGORY,
+        QUESTION_IDN,
+        QUESTION_ENG,
+        ANSWER_IDN,
+        ANSWER_ENG,
+        KEYWORDS,
+        LIKES,
+        DISLIKES,
+        SEEN_BY_USER,
+        CREATED_BY,
+        CREATED_DATE,
+        MODIFIED_BY,
+        MODIFIED_DATE,
+        IS_DELETED,
+        STATUS
+    )
+VALUES(
+        801,
+        'Berapa lama waktu investasi reksa dana?',
+        'Berapa lama waktu investasi reksa dana?',
+        'Tidak ada jangka waktu minimum. Nasabah dapat melakukan pembelian dan penjualan unit kapanpun. <br/>Sebagai edukasi: Sebaiknya investasi Reksa Dana dilakukan untuk rencana jangka waktu menengah hingga panjang.',
+        'Tidak ada jangka waktu minimum. Nasabah dapat melakukan pembelian dan penjualan unit kapanpun. <br/>Sebagai edukasi: Sebaiknya investasi Reksa Dana dilakukan untuk rencana jangka waktu menengah hingga panjang.',
+        'Reksa Dana',
+        0,
+        0,
+        0,
+        'ADMINPORTAL',
+        TIMESTAMP '2023-05-15 03:59:55.533',
+        NULL,
+        NULL,
+        0,
+        0
+    );
