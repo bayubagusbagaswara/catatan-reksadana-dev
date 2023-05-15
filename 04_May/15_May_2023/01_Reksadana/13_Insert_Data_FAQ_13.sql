39cd8d568eb53f566693de566870b6f20df2620a
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-05-15 10:15:41 +0700

    insert data faq 13

---
+++ 04_May/15_May_2023/01_Reksadana/13_Insert_Data_FAQ_13.sql
@@ -0,0 +1,34 @@
+INSERT INTO PARAMETER_MANAGEMENT_SERVICE.FAQ (
+        ID_FAQ_CATEGORY,
+        QUESTION_IDN,
+        QUESTION_ENG,
+        ANSWER_IDN,
+        ANSWER_ENG,
+        KEYWORDS,
+        LIKES,
+        DISLIKES,
+        SEEN_BY_USER,
+        CREATED_BY,
+        CREATED_DATE,
+        MODIFIED_BY,
+        MODIFIED_DATE,
+        IS_DELETED,
+        STATUS
+    )
+VALUES(
+        801,
+        'Apakah yang dimaksud dengan produk reksa dana pasar campuran?',
+        'Apakah yang dimaksud dengan produk reksa dana pasar campuran?',
+        'Produk yang penempatan investasinya menggunakan Porsi berimbang antara saham dan sukuk sehingga berfluktuasi moderat. Risiko produk ini sedang dan potensi hasil sedang. Reksa Dana Campuran baik untuk investor moderat (3–5 tahun) yang ingin menikmati potensi dari saham dan sukuk.',
+        'Produk yang penempatan investasinya menggunakan Porsi berimbang antara saham dan sukuk sehingga berfluktuasi moderat. Risiko produk ini sedang dan potensi hasil sedang. Reksa Dana Campuran baik untuk investor moderat (3–5 tahun) yang ingin menikmati potensi dari saham dan sukuk.',
+        'Reksa Dana',
+        0,
+        0,
+        0,
+        'ADMINPORTAL',
+        TIMESTAMP '2023-05-15 04:14:40.283',
+        NULL,
+        NULL,
+        0,
+        0
+    );
