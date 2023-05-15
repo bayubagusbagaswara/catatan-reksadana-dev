e7352671bb95b9e4801d8de573330f3cb0dda301
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-05-15 09:47:54 +0700

    add insert data faq 2

---
+++ 04_May/15_May_2023/01_Reksadana/02_Insert_Data_FAQ_2.sql
@@ -0,0 +1,34 @@
+INSERT INTO PARAMETER_MANAGEMENT_SERVICE.FAQ (
+        ID_FAQ_CATEGORY,
+        QUESTION_IDN,
+        QUESTION_ENG,
+        ANSWER_IDN,
+        ANSWER_ENG,
+        KEYWORDS,
+        LIKES,
+        DISLIKES,
+        SEEN_BY_USER,
+        CREATED_BY,
+        CREATED_DATE,
+        MODIFIED_BY,
+        MODIFIED_DATE,
+        IS_DELETED,
+        STATUS
+    )
+VALUES(
+        801,
+        'Apa keunggulan reksa dana bagi nasabah?',
+        'Apa keunggulan reksa dana bagi nasabah?',
+        'Fleksibel (Banyak pilihan produk sesuai kebutuhan investasi)<br/>Likuid (Bisa dicairkan kapan saja, tanpa jangka waktu)<br/>Aman (Terdaftar dan diawasi OJK)<br/>Bebas pajak (Hasil investasi reksa dana tidak dipotong pajak)',
+        'Fleksibel (Banyak pilihan produk sesuai kebutuhan investasi)<br/>Likuid (Bisa dicairkan kapan saja, tanpa jangka waktu)<br/>Aman (Terdaftar dan diawasi OJK)<br/>Bebas pajak (Hasil investasi reksa dana tidak dipotong pajak)',
+        'Reksa Dana',
+        0,
+        0,
+        0,
+        'ADMINPORTAL',
+        TIMESTAMP '2023-05-15 03:43:29.927',
+        NULL,
+        NULL,
+        0,
+        0
+    );
