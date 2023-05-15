3ad66add015813b9b972b620ddfc07cf28a1e031
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-05-15 10:37:00 +0700

    insert data faq 7

---
+++ 04_May/15_May_2023/02_Registrasi/07_Insert_Data_FAQ_7.sql
@@ -0,0 +1,34 @@
+INSERT INTO PARAMETER_MANAGEMENT_SERVICE.FAQ (
+        ID_FAQ_CATEGORY,
+        QUESTION_IDN,
+        QUESTION_ENG,
+        ANSWER_IDN,
+        ANSWER_ENG,
+        KEYWORDS,
+        LIKES,
+        DISLIKES,
+        SEEN_BY_USER,
+        CREATED_BY,
+        CREATED_DATE,
+        MODIFIED_BY,
+        MODIFIED_DATE,
+        IS_DELETED,
+        STATUS
+    )
+VALUES(
+        821,
+        'Bagaimana cara mengetahui produk-produk reksa dana sebelum melakukan pendaftaran?',
+        'Bagaimana cara mengetahui produk-produk reksa dana sebelum melakukan pendaftaran?',
+        'Nasabah dapat melakukan eksplorasi produk reksadana untuk mengetahui kinerja produk dan manajer investasinya dan meninjau dokumen prospectus serta fund fact sheet',
+        'Nasabah dapat melakukan eksplorasi produk reksadana untuk mengetahui kinerja produk dan manajer investasinya dan meninjau dokumen prospectus serta fund fact sheet',
+        'Reksa Dana, Registrasi',
+        0,
+        0,
+        0,
+        'ADMINPORTAL',
+        TIMESTAMP '2023-05-15 04:36:26.697',
+        NULL,
+        NULL,
+        0,
+        0
+    );
