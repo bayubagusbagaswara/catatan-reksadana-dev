7226ae6864dd1382c22b8b5c6697c6ec1082f1a4
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-05-15 09:56:26 +0700

    insert data faq 5

---
+++ 04_May/15_May_2023/01_Reksadana/05_Insert_Data_FAQ_5.sql
@@ -0,0 +1,34 @@
+INSERT INTO PARAMETER_MANAGEMENT_SERVICE.FAQ (
+        ID_FAQ_CATEGORY,
+        QUESTION_IDN,
+        QUESTION_ENG,
+        ANSWER_IDN,
+        ANSWER_ENG,
+        KEYWORDS,
+        LIKES,
+        DISLIKES,
+        SEEN_BY_USER,
+        CREATED_BY,
+        CREATED_DATE,
+        MODIFIED_BY,
+        MODIFIED_DATE,
+        IS_DELETED,
+        STATUS
+    )
+VALUES(
+        801,
+        'Apa saja jenis produk reksa dana?',
+        'Apa saja jenis produk reksa dana?',
+        'Reksa Dana Pasar Uang, Pendapatan Tetap, Campuran, dan Saham',
+        'Reksa Dana Pasar Uang, Pendapatan Tetap, Campuran, dan Saham',
+        'Reksa Dana',
+        0,
+        0,
+        0,
+        'ADMINPORTAL',
+        TIMESTAMP '2023-05-15 03:55:13.210',
+        NULL,
+        NULL,
+        0,
+        0
+    );
