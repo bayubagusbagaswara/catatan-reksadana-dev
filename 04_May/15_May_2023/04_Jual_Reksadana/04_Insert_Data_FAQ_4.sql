98e22d26e610c41a0fba399960e49d05ad844d8d
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-05-15 10:58:39 +0700

    insert data faq 4

---
+++ 04_May/15_May_2023/04_Jual_Reksadana/04_Insert_Data_FAQ_4.sql
@@ -0,0 +1,34 @@
+INSERT INTO PARAMETER_MANAGEMENT_SERVICE.FAQ (
+        ID_FAQ_CATEGORY,
+        QUESTION_IDN,
+        QUESTION_ENG,
+        ANSWER_IDN,
+        ANSWER_ENG,
+        KEYWORDS,
+        LIKES,
+        DISLIKES,
+        SEEN_BY_USER,
+        CREATED_BY,
+        CREATED_DATE,
+        MODIFIED_BY,
+        MODIFIED_DATE,
+        IS_DELETED,
+        STATUS
+    )
+VALUES(
+        823,
+        'Apakah ada biaya pada proses pengajuan penjualan reksa dana?',
+        'Apakah ada biaya pada proses pengajuan penjualan reksa dana?',
+        'Untuk Reksa Dana Pasar Uang, nasabah tidak dikenakan biaya penjualan (redemption fee). Namun, untuk Reksa Dana non Pasar Uang, nasabah dikenakan biaya pembelian sebesar 1% tergantung produk Reksa Dana yang dijual nasabah',
+        'Untuk Reksa Dana Pasar Uang, nasabah tidak dikenakan biaya penjualan (redemption fee). Namun, untuk Reksa Dana non Pasar Uang, nasabah dikenakan biaya pembelian sebesar 1% tergantung produk Reksa Dana yang dijual nasabah',
+        'Reksa Dana, Jual',
+        0,
+        0,
+        0,
+        'ADMINPORTAL',
+        TIMESTAMP '2023-05-15 04:57:11.870',
+        NULL,
+        NULL,
+        0,
+        0
+    );
