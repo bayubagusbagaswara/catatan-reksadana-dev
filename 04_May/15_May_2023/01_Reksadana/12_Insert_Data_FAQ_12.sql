d3524495d67aaef80fc7da2ba22519e7fd01dd08
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-05-15 10:13:20 +0700

    insert data faq 12

---
+++ 04_May/15_May_2023/01_Reksadana/12_Insert_Data_FAQ_12.sql
@@ -0,0 +1,34 @@
+INSERT INTO PARAMETER_MANAGEMENT_SERVICE.FAQ (
+        ID_FAQ_CATEGORY,
+        QUESTION_IDN,
+        QUESTION_ENG,
+        ANSWER_IDN,
+        ANSWER_ENG,
+        KEYWORDS,
+        LIKES,
+        DISLIKES,
+        SEEN_BY_USER,
+        CREATED_BY,
+        CREATED_DATE,
+        MODIFIED_BY,
+        MODIFIED_DATE,
+        IS_DELETED,
+        STATUS
+    )
+VALUES(
+        801,
+        'Apakah yang dimaksud dengan produk reksa dana pendapatan tetap?',
+        'Apakah yang dimaksud dengan produk reksa dana pendapatan tetap?',
+        'Produk yang komposisi penempatan investasinya adalah minimal 80% Sukuk, sehingga tidak terlalu berfluktuasi. Risiko produk ini rendah dan potensi hasil juga relatif rendah. Reksa Dana Pendapatan Tetap baik untuk investor konservatif (1-3 tahun) dengan jangka waktu investasi pendek',
+        'Produk yang komposisi penempatan investasinya adalah minimal 80% Sukuk, sehingga tidak terlalu berfluktuasi. Risiko produk ini rendah dan potensi hasil juga relatif rendah. Reksa Dana Pendapatan Tetap baik untuk investor konservatif (1-3 tahun) dengan jangka waktu investasi pendek',
+        'Reksa Dana',
+        0,
+        0,
+        0,
+        'ADMINPORTAL',
+        TIMESTAMP '2023-05-15 04:11:57.327',
+        NULL,
+        NULL,
+        0,
+        0
+    );
