952c5ff95054659200eee26618e15deb58df01e3
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-05-15 10:33:58 +0700

    insert data faq 5

---
+++ 04_May/15_May_2023/02_Registrasi/05_Insert_Data_FAQ_5.sql
@@ -0,0 +1,34 @@
+INSERT INTO PARAMETER_MANAGEMENT_SERVICE.FAQ (
+        ID_FAQ_CATEGORY,
+        QUESTION_IDN,
+        QUESTION_ENG,
+        ANSWER_IDN,
+        ANSWER_ENG,
+        KEYWORDS,
+        LIKES,
+        DISLIKES,
+        SEEN_BY_USER,
+        CREATED_BY,
+        CREATED_DATE,
+        MODIFIED_BY,
+        MODIFIED_DATE,
+        IS_DELETED,
+        STATUS
+    )
+VALUES(
+        821,
+        'Bagaimana proses registrasi bagi nasabah yang sudah memiliki SID dan IFUA (telah melakukan transaksi secara offline?',
+        'Bagaimana proses registrasi bagi nasabah yang sudah memiliki SID dan IFUA (telah melakukan transaksi secara offline?',
+        'Proses registrasi tidak akan dikenakan bagi nasabah yang telah memiliki SID dan IFUA BSI, nasabah dapat langsung melakukan transaksi apabila sudah memiliki SID dan IFUA',
+        'Proses registrasi tidak akan dikenakan bagi nasabah yang telah memiliki SID dan IFUA BSI, nasabah dapat langsung melakukan transaksi apabila sudah memiliki SID dan IFUA',
+        'Reksa Dana, Registrasi',
+        0,
+        0,
+        0,
+        'ADMINPORTAL',
+        TIMESTAMP '2023-05-15 04:32:15.087',
+        NULL,
+        NULL,
+        0,
+        0
+    );
