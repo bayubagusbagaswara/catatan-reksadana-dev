e151926f016a9ba8a756eabbe6ba43d98a06290f
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-05-15 11:02:10 +0700

    insert data faq 2

---
+++ 04_May/15_May_2023/05_Portfolio/02_Insert_Data_FAQ_2.sql
@@ -0,0 +1,34 @@
+INSERT INTO PARAMETER_MANAGEMENT_SERVICE.FAQ (
+        ID_FAQ_CATEGORY,
+        QUESTION_IDN,
+        QUESTION_ENG,
+        ANSWER_IDN,
+        ANSWER_ENG,
+        KEYWORDS,
+        LIKES,
+        DISLIKES,
+        SEEN_BY_USER,
+        CREATED_BY,
+        CREATED_DATE,
+        MODIFIED_BY,
+        MODIFIED_DATE,
+        IS_DELETED,
+        STATUS
+    )
+VALUES(
+        824,
+        'Apakah portfolio transaksi dicabang juga akan terakumulasi pada portfolio mobile banking?',
+        'Apakah portfolio transaksi dicabang juga akan terakumulasi pada portfolio mobile banking?',
+        'Iya, Portfolio yang muncul di mobile banking adalah keseluruhan portofolio nasabah baik yang dibeli via mobile banking maupun melalui cabang',
+        'Iya, Portfolio yang muncul di mobile banking adalah keseluruhan portofolio nasabah baik yang dibeli via mobile banking maupun melalui cabang',
+        'Reksa Dana, Portfolio',
+        0,
+        0,
+        0,
+        'ADMINPORTAL',
+        TIMESTAMP '2023-05-15 05:01:32.870',
+        NULL,
+        NULL,
+        0,
+        0
+    );
