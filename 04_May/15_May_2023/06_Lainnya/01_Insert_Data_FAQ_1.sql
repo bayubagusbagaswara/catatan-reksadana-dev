42730b16f50cfe4145da16d159099579feec2abf
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-05-15 11:03:56 +0700

    insert data faq 1

---
+++ 04_May/15_May_2023/06_Lainnya/01_Insert_Data_FAQ_1.sql
@@ -0,0 +1,34 @@
+INSERT INTO PARAMETER_MANAGEMENT_SERVICE.FAQ (
+        ID_FAQ_CATEGORY,
+        QUESTION_IDN,
+        QUESTION_ENG,
+        ANSWER_IDN,
+        ANSWER_ENG,
+        KEYWORDS,
+        LIKES,
+        DISLIKES,
+        SEEN_BY_USER,
+        CREATED_BY,
+        CREATED_DATE,
+        MODIFIED_BY,
+        MODIFIED_DATE,
+        IS_DELETED,
+        STATUS
+    )
+VALUES(
+        825,
+        'Apa yang harus dilakukan nasabah apabila SID Nasabah di blokir?',
+        'Apa yang harus dilakukan nasabah apabila SID Nasabah di blokir?',
+        'Nasabah diarahkan melakukan pengaktifan dengan melakukan kontak ke Call Center atau Cabang BSI',
+        'Nasabah diarahkan melakukan pengaktifan dengan melakukan kontak ke Call Center atau Cabang BSI',
+        'Reksa Dana, Lainnya',
+        0,
+        0,
+        0,
+        'ADMINPORTAL',
+        TIMESTAMP '2023-05-15 05:03:26.767',
+        NULL,
+        NULL,
+        0,
+        0
+    );
