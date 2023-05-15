648ef3b65abdadbf95b2fdce001660b3f95e284b
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-05-15 10:47:53 +0700

    insert data faq 4

---
+++ 04_May/15_May_2023/03_Beli_Reksadana/04_Insert_Data_FAQ_4.sql
@@ -0,0 +1,34 @@
+INSERT INTO PARAMETER_MANAGEMENT_SERVICE.FAQ (
+        ID_FAQ_CATEGORY,
+        QUESTION_IDN,
+        QUESTION_ENG,
+        ANSWER_IDN,
+        ANSWER_ENG,
+        KEYWORDS,
+        LIKES,
+        DISLIKES,
+        SEEN_BY_USER,
+        CREATED_BY,
+        CREATED_DATE,
+        MODIFIED_BY,
+        MODIFIED_DATE,
+        IS_DELETED,
+        STATUS
+    )
+VALUES(
+        822,
+        'Apakah ada biaya pada proses pengajuan pembelian reksa dana?',
+        'Apakah ada biaya pada proses pengajuan pembelian reksa dana?',
+        'Untuk Reksa Dana Pasar Uang, nasabah tidak dikenakan biaya pembelian (subscription fee). Namun, untuk Reksa Dana non Pasar Uang, nasabah dikenakan biaya pembelian sebesar 1%-1,5% tergantung produk Reksa Dana yang dibeli nasabah.',
+        'Untuk Reksa Dana Pasar Uang, nasabah tidak dikenakan biaya pembelian (subscription fee). Namun, untuk Reksa Dana non Pasar Uang, nasabah dikenakan biaya pembelian sebesar 1%-1,5% tergantung produk Reksa Dana yang dibeli nasabah.',
+        'Reksa Dana, Beli',
+        0,
+        0,
+        0,
+        'ADMINPORTAL',
+        TIMESTAMP '2023-05-15 04:47:12.427',
+        NULL,
+        NULL,
+        0,
+        0
+    );
