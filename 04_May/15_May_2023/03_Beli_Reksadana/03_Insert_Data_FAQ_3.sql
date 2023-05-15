06adb333c36ec47216570214835cc2b7e54cb00c
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-05-15 10:46:31 +0700

    insert data faq 3

---
+++ 04_May/15_May_2023/03_Beli_Reksadana/03_Insert_Data_FAQ_3.sql
@@ -0,0 +1,34 @@
+INSERT INTO PARAMETER_MANAGEMENT_SERVICE.FAQ (
+        ID_FAQ_CATEGORY,
+        QUESTION_IDN,
+        QUESTION_ENG,
+        ANSWER_IDN,
+        ANSWER_ENG,
+        KEYWORDS,
+        LIKES,
+        DISLIKES,
+        SEEN_BY_USER,
+        CREATED_BY,
+        CREATED_DATE,
+        MODIFIED_BY,
+        MODIFIED_DATE,
+        IS_DELETED,
+        STATUS
+    )
+VALUES(
+        822,
+        'Berapa lama waktu yang diperlukan untuk pengajuan pembelian reksa dana?',
+        'Berapa lama waktu yang diperlukan untuk pengajuan pembelian reksa dana?',
+        'Lama waktu (SLA) pengajuan pembelian maksimal 3 Hari Bursa (Hari Kerja KSEI)',
+        'Lama waktu (SLA) pengajuan pembelian maksimal 3 Hari Bursa (Hari Kerja KSEI)',
+        'Reksa Dana, Beli',
+        0,
+        0,
+        0,
+        'ADMINPORTAL',
+        TIMESTAMP '2023-05-15 04:45:43.290',
+        NULL,
+        NULL,
+        0,
+        0
+    );
