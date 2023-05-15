55f79ccf66edcdbb588e2cdd411e2f425c9554bb
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-05-15 10:56:33 +0700

    insert data faq 3

---
+++ 04_May/15_May_2023/04_Jual_Reksadana/03_Insert_Data_FAQ_3.sql
@@ -0,0 +1,34 @@
+INSERT INTO PARAMETER_MANAGEMENT_SERVICE.FAQ (
+        ID_FAQ_CATEGORY,
+        QUESTION_IDN,
+        QUESTION_ENG,
+        ANSWER_IDN,
+        ANSWER_ENG,
+        KEYWORDS,
+        LIKES,
+        DISLIKES,
+        SEEN_BY_USER,
+        CREATED_BY,
+        CREATED_DATE,
+        MODIFIED_BY,
+        MODIFIED_DATE,
+        IS_DELETED,
+        STATUS
+    )
+VALUES(
+        823,
+        'Berapa lama waktu yang diperlukan hingga dana penjualan Reksa Dana diterima oleh nasabah?',
+        'Berapa lama waktu yang diperlukan hingga dana penjualan Reksa Dana diterima oleh nasabah?',
+        'Lama waktu (SLA) pengajuan pembelian maksimal 7 Hari Bursa (Hari Kerja KSEI), namun untuk SLA pencairan dana tiap produk berbeda-beda',
+        'Lama waktu (SLA) pengajuan pembelian maksimal 7 Hari Bursa (Hari Kerja KSEI), namun untuk SLA pencairan dana tiap produk berbeda-beda',
+        'Reksa Dana, Jual',
+        0,
+        0,
+        0,
+        'ADMINPORTAL',
+        TIMESTAMP '2023-05-15 04:56:00.977',
+        NULL,
+        NULL,
+        0,
+        0
+    );
