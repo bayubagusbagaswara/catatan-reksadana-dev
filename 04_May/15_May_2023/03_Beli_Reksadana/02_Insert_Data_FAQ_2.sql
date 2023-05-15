c11e36755080cf04ce10718af830fac657113b57
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-05-15 10:43:48 +0700

    insert data faq 2

---
+++ 04_May/15_May_2023/03_Beli_Reksadana/02_Insert_Data_FAQ_2.sql
@@ -0,0 +1,34 @@
+INSERT INTO PARAMETER_MANAGEMENT_SERVICE.FAQ (
+        ID_FAQ_CATEGORY,
+        QUESTION_IDN,
+        QUESTION_ENG,
+        ANSWER_IDN,
+        ANSWER_ENG,
+        KEYWORDS,
+        LIKES,
+        DISLIKES,
+        SEEN_BY_USER,
+        CREATED_BY,
+        CREATED_DATE,
+        MODIFIED_BY,
+        MODIFIED_DATE,
+        IS_DELETED,
+        STATUS
+    )
+VALUES(
+        822,
+        'Apa saja poin informasi yang dapat diperhatikan nasabah ketika melakukan proses pembelian reksa dana?',
+        'Apa saja poin informasi yang dapat diperhatikan nasabah ketika melakukan proses pembelian reksa dana?',
+        'Nasabah dapat mengetahui informasi ketentuan produk, kinerja produk dalam kurun waktu tertentu, dan informasi detail performa manajer investasi pada dokumen prospectus serta fund fact sheet dan NAV (harga reksadana)',
+        'Nasabah dapat mengetahui informasi ketentuan produk, kinerja produk dalam kurun waktu tertentu, dan informasi detail performa manajer investasi pada dokumen prospectus serta fund fact sheet dan NAV (harga reksadana)',
+        'Reksa Dana, Beli',
+        0,
+        0,
+        0,
+        'ADMINPORTAL',
+        TIMESTAMP '2023-05-15 04:43:17.670',
+        NULL,
+        NULL,
+        0,
+        0
+    );
