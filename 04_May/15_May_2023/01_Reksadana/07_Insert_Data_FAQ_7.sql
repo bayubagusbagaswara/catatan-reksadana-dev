0ae7c2f8c16888e4d1a36b0caa6aaa3606eed5fb
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-05-15 09:59:37 +0700

    insert data faq 7

---
+++ 04_May/15_May_2023/01_Reksadana/07_Insert_Data_FAQ_7.sql
@@ -0,0 +1,34 @@
+INSERT INTO PARAMETER_MANAGEMENT_SERVICE.FAQ (
+        ID_FAQ_CATEGORY,
+        QUESTION_IDN,
+        QUESTION_ENG,
+        ANSWER_IDN,
+        ANSWER_ENG,
+        KEYWORDS,
+        LIKES,
+        DISLIKES,
+        SEEN_BY_USER,
+        CREATED_BY,
+        CREATED_DATE,
+        MODIFIED_BY,
+        MODIFIED_DATE,
+        IS_DELETED,
+        STATUS
+    )
+VALUES(
+        801,
+        'Apa saja risiko melakukan investasi reksa dana?',
+        'Apa saja risiko melakukan investasi reksa dana?',
+        'Risiko Likuiditas yaitu penundaan pencairan dana <br/>Risiko berkurangnya nilai investasi <br/>Risiko Perubahan Kondisi Politik dan Ekonomi <br/>Risiko Wanprestasi yaitu kondisi dimana penerbit efek tidak dapat memenuhi kewajibannya',
+        'Risiko Likuiditas yaitu penundaan pencairan dana <br/>Risiko berkurangnya nilai investasi <br/>Risiko Perubahan Kondisi Politik dan Ekonomi <br/>Risiko Wanprestasi yaitu kondisi dimana penerbit efek tidak dapat memenuhi kewajibannya',
+        'Reksa Dana',
+        0,
+        0,
+        0,
+        'ADMINPORTAL',
+        TIMESTAMP '2023-05-15 03:58:15.960',
+        NULL,
+        NULL,
+        0,
+        0
+    );
