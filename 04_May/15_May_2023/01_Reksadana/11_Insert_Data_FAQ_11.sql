e8de2d031eacbf5e4ce58c927e7aac95a4db71a6
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-05-15 10:11:42 +0700

    insert data faq 11

---
+++ 04_May/15_May_2023/01_Reksadana/11_Insert_Data_FAQ_11.sql
@@ -0,0 +1,34 @@
+INSERT INTO PARAMETER_MANAGEMENT_SERVICE.FAQ (
+        ID_FAQ_CATEGORY,
+        QUESTION_IDN,
+        QUESTION_ENG,
+        ANSWER_IDN,
+        ANSWER_ENG,
+        KEYWORDS,
+        LIKES,
+        DISLIKES,
+        SEEN_BY_USER,
+        CREATED_BY,
+        CREATED_DATE,
+        MODIFIED_BY,
+        MODIFIED_DATE,
+        IS_DELETED,
+        STATUS
+    )
+VALUES(
+        801,
+        'Apakah yang dimaksud dengan produk reksa dana pasar uang?',
+        'Apakah yang dimaksud dengan produk reksa dana pasar uang?',
+        'Produk yang penempatan investasinya hanya pada instrumen pasar uang berjangka (<1 tahun) dan deposito. Risiko produk ini sangat kecil. Reksa Dana ini sangat baik untuk investor yang sangat konservatif dengan tujuan investasi jangka pendek (0-1 tahun), dan baru mulai belajar berinvestasi.',
+        'Produk yang penempatan investasinya hanya pada instrumen pasar uang berjangka (<1 tahun) dan deposito. Risiko produk ini sangat kecil. Reksa Dana ini sangat baik untuk investor yang sangat konservatif dengan tujuan investasi jangka pendek (0-1 tahun), dan baru mulai belajar berinvestasi.',
+        'Reksa Dana',
+        0,
+        0,
+        0,
+        'ADMINPORTAL',
+        TIMESTAMP '2023-05-15 04:08:56.193',
+        NULL,
+        NULL,
+        0,
+        0
+    );
