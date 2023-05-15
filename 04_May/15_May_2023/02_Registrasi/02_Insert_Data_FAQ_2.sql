3cbb6fc3dc0563011488ee9b6da6d4499c5b69cc
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-05-15 10:28:42 +0700

    insert data faq 2

---
+++ 04_May/15_May_2023/02_Registrasi/02_Insert_Data_FAQ_2.sql
@@ -0,0 +1,34 @@
+INSERT INTO PARAMETER_MANAGEMENT_SERVICE.FAQ (
+        ID_FAQ_CATEGORY,
+        QUESTION_IDN,
+        QUESTION_ENG,
+        ANSWER_IDN,
+        ANSWER_ENG,
+        KEYWORDS,
+        LIKES,
+        DISLIKES,
+        SEEN_BY_USER,
+        CREATED_BY,
+        CREATED_DATE,
+        MODIFIED_BY,
+        MODIFIED_DATE,
+        IS_DELETED,
+        STATUS
+    )
+VALUES(
+        821,
+        'Untuk apa dilakukan proses registrasi?',
+        'Untuk apa dilakukan proses registrasi?',
+        'Proses registrasi diperlukan untuk nasabah yang belum memiliki SID dan IFUA',
+        'Proses registrasi diperlukan untuk nasabah yang belum memiliki SID dan IFUA',
+        'Reksa Dana, Registrasi',
+        0,
+        0,
+        0,
+        'ADMINPORTAL',
+        TIMESTAMP '2023-05-15 04:27:56.890',
+        NULL,
+        NULL,
+        0,
+        0
+    );
