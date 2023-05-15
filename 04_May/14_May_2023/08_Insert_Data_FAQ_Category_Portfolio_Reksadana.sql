c98674bc9941d0921d0709064d4673a351b5ea84
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-05-15 09:25:50 +0700

    add insert data faq category portfolio reksadana

---
+++ 04_May/14_May_2023/08_Insert_Data_FAQ_Category_Portfolio_Reksadana.sql
@@ -0,0 +1,28 @@
+INSERT INTO PARAMETER_MANAGEMENT_SERVICE.FAQ_CATEGORY (
+        CREATED_BY,
+        CREATED_DATE,
+        MODIFIED_BY,
+        MODIFIED_DATE,
+        NAME_IDN,
+        NAME_ENG,
+        IMAGE_URL,
+        PRIORITY,
+        IS_DELETED,
+        STATUS,
+        CATEGORY_CODE,
+        MODULE
+    )
+VALUES(
+        'ADMIN',
+        TIMESTAMP '2023-05-15 03:17:24.490',
+        NULL,
+        NULL,
+        'Portfolio',
+        'Portfolio',
+        'http://mf8.myinfosys.net:7005/bsi/assets/FaqCategory/FaqCategoryQCV6ThuMay0410:50:22WIB2023.png',
+        4,
+        0,
+        '0',
+        'PORTFOLIO',
+        'REKSADANA'
+    );
