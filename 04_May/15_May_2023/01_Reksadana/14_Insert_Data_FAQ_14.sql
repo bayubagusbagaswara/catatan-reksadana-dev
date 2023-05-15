8a43e896ee7470ecc47be252d6ce0213fe3d5482
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-05-15 10:17:10 +0700

    insert data faq 14

---
+++ 04_May/15_May_2023/01_Reksadana/14_Insert_Data_FAQ_14.sql
@@ -0,0 +1,34 @@
+INSERT INTO PARAMETER_MANAGEMENT_SERVICE.FAQ (
+        ID_FAQ_CATEGORY,
+        QUESTION_IDN,
+        QUESTION_ENG,
+        ANSWER_IDN,
+        ANSWER_ENG,
+        KEYWORDS,
+        LIKES,
+        DISLIKES,
+        SEEN_BY_USER,
+        CREATED_BY,
+        CREATED_DATE,
+        MODIFIED_BY,
+        MODIFIED_DATE,
+        IS_DELETED,
+        STATUS
+    )
+VALUES(
+        801,
+        'Apakah yang dimaksud dengan produk reksa dana saham?',
+        'Apakah yang dimaksud dengan produk reksa dana saham?',
+        'Produk yang penempatan investasinya adalah minimal 80% saham sehingga berfluktuasi dengan agresif. Risiko produk ini tinggi dan potensi hasil juga tinggi. Reksa Dana Saham baik untuk investasi jangka panjang (5->10 tahun).',
+        'Produk yang penempatan investasinya adalah minimal 80% saham sehingga berfluktuasi dengan agresif. Risiko produk ini tinggi dan potensi hasil juga tinggi. Reksa Dana Saham baik untuk investasi jangka panjang (5->10 tahun).',
+        'Reksa Dana',
+        0,
+        0,
+        0,
+        'ADMINPORTAL',
+        TIMESTAMP '2023-05-15 04:16:30.800',
+        NULL,
+        NULL,
+        0,
+        0
+    );
