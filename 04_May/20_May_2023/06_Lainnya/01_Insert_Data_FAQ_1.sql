c86dcb6ffa0510d379503050fe88cf37c701df29
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-05-21 13:29:20 +0700

    add lainnya reksadana faq

---
+++ 04_May/20_May_2023/06_Lainnya/01_Insert_Data_FAQ_1.sql
@@ -0,0 +1,24 @@
+INSERT INTO FAQ (
+        faq_category_code,
+        faq_code,
+        question,
+        answer,
+        popular_order,
+        created_by,
+        created_date,
+        modified_by,
+        modified_date,
+        is_deleted
+    )
+VALUES(
+        'LAINNYA',
+        'LAINNYA_FAQ_1',
+        'Apa yang harus dilakukan nasabah apabila SID Nasabah di blokir?',
+        'Nasabah diarahkan melakukan pengaktifan dengan melakukan kontak ke Call Center atau Cabang BSI',
+        36,
+        'SYSTEM',
+        TIMESTAMP '2023-05-15 05:03:26.767',
+        NULL,
+        NULL,
+        FALSE
+    );
---
+++ 04_May/20_May_2023/06_Lainnya/02_Insert_Data_FAQ_2.sql
@@ -0,0 +1,24 @@
+INSERT INTO FAQ (
+        faq_category_code,
+        faq_code,
+        question,
+        answer,
+        popular_order,
+        created_by,
+        created_date,
+        modified_by,
+        modified_date,
+        is_deleted
+    )
+VALUES(
+        'LAINNYA',
+        'LAINNYA_FAQ_2',
+        'Bagaimana jika profil risiko nasabah expired?',
+        'Nasabah akan mendapatkan pop up transaksi apabila profil risiko expired dan nasabah diarahkan melakukan pengisian profil risiko',
+        37,
+        'SYSTEM',
+        TIMESTAMP '2023-05-15 05:04:33.707',
+        NULL,
+        NULL,
+        FALSE
+    );
