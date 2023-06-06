48fcd1281ffb11edca57965caffa3cb76ec5c2fc
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-06-06 10:41:39 +0700

    question

---
+++ 04_May/29_May_2023/01_Question_1.sql
@@ -0,0 +1,22 @@
+INSERT INTO RISK_PROFILE_QUESTION(
+        number,
+        name,
+        type,
+        language,
+        created_by,
+        created_date,
+        modified_by,
+        modified_date,
+        is_deleted
+    )
+VALUES(
+        '1',
+        'Berapa usia kamu saat ini?',
+        NULL,
+        'IDN',
+        'SYSTEM',
+        current_timestamp,
+        NULL,
+        NULL,
+        FALSE
+    );
