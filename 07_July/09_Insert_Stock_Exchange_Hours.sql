80842f3413dd188e15c082323f22e97eedf80fe1
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-07-07 20:46:59 +0700

    insert stock exchange hours

---
+++ 07_July/09_Insert_Stock_Exchange_Hours.sql
@@ -0,0 +1,24 @@
+INSERT INTO PARAMETER_MANAGEMENT_SERVICE.SYSTEM_PARAMETER (
+        CREATED_BY,
+        CREATED_DATE,
+        DESCRIPTION,
+        IS_ENCRYPTED,
+        MODIFIED_BY,
+        MODIFIED_DATE,
+        MODULE,
+        NAME,
+        VALUE,
+        IS_DELETED
+    )
+VALUES (
+        'SYSTEM',
+        TIMESTAMP '2023-07-06 10:55:00',
+        'Working Hours',
+        1,
+        'SYSTEM',
+        TIMESTAMP '2023-07-06 10:55:00',
+        'INVEST_REKSADANA',
+        'WORKING_HOURS',
+        '09:00 - 16:00',
+        0
+    );
