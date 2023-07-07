1ad0fcc995636e45a47227823bec18ec7add1744
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-07-07 20:46:31 +0700

    insert content parameter subscription on process

---
+++ 07_July/08_Insert_Content_Paramter_Subscription_On_Process.sql
@@ -0,0 +1,189 @@
+INSERT INTO PARAMETER_MANAGEMENT_SERVICE.CONTENT_PARAMETER (
+        CREATED_BY,
+        CREATED_DATE,
+        DESCRIPTION,
+        MODIFIED_BY,
+        MODIFIED_DATE,
+        MODULE,
+        NAME,
+        VALUE,
+        "LANGUAGE",
+        "ORDERING",
+        LABEL1,
+        LABEL2,
+        IMAGE_URL,
+        ADDITIONAL_DATA
+    )
+VALUES (
+        'SYSTEM',
+        TIMESTAMP '2023-07-06 10:15:43.018',
+        'Template Receipt IDN Subscription Reksa Dana On Process',
+        NULL,
+        NULL,
+        'TEMPLATE_RECEIPT',
+        'REKSADANA_SUBSCRIPTION_ON_PROCESS',
+        '-',
+        'IDN',
+        1,
+        NULL,
+        NULL,
+        NULL,
+        TO_CLOB(
+            '{
+  "transactionStatusData": [
+    {
+      "value": "<font color=\"#F4000000\" size=\"16\" weight=\"bold\">${namaNasabah}</font>",
+      "key": "Nama Nasabah",
+      "type": "data",
+      "background": "#00FFFFFF"
+    },
+    {
+      "value": "",
+      "key": "",
+      "type": "line"
+    },
+     {
+      "value": "<font color=\"#F4000000\" size=\"16\" weight=\"bold\">${email}</font>",
+      "key": "Alamat Email",
+      "type": "data",
+      "background": "#00FFFFFF"
+    },
+   
+    {
+      "value": "",
+      "key": "",
+      "type": "line"
+    },
+    {
+      "value": "<font color=\"#F4000000\" size=\"14\" weight=\"normal\">${riskProfile}</font>",
+      "key": "Profil Risiko",
+      "type": "data",
+      "background": "#00FFFFFF"
+    },
+    {
+      "detailTransaksi": [
+        {
+          "value": "<font color=\"#F4000000\" size=\"14\" weight=\"normal\">${namaNasabah}</font>",
+          "key": "Nama Nasabah",
+          "type": "data",
+          "background": "#00FFFFFF"
+        },
+        {
+          "value": "",
+          "key": "",
+          "type": "line"
+        },
+        {
+          "value": "<font color=\"#F4000000\" size=\"14\" weight=\"normal\">${email}</font>",
+          "key": "Alamat Email",
+          "type": "data",
+          "background": "#00FFFFFF"
+        },
+        {
+          "value": "",
+          "key": "",
+          "type": "line"
+        },
+        {
+          "value": "<font color=\"#F4000000\" size=\"14\" weight=\"normal\">${nik}</font>",
+          "key": "NIK",
+          "type": "data",
+          "background": "#00FFFFFF"
+        },
+        {
+          "value": "",
+          "key": "",
+          "type": "line"
+        }'
+        ) || TO_CLOB(
+            ',
+        {
+          "value": "<font color=\"#F4000000\" size=\"14\" weight=\"normal\">${riskProfile}</font>",
+          "key": "Profil Risiko",
+          "type": "data",
+          "background": "#00FFFFFF"
+        },
+        {
+          "value": "",
+          "key": "",
+          "type": "line"
+        },
+        {
+          "value": "<font color=\"#F4000000\" size=\"14\" weight=\"normal\">${tanggal}</font>",
+          "key": "Tanggal",
+          "type": "data",
+          "background": "#00FFFFFF"
+        },
+        {
+          "value": "",
+          "key": "",
+          "type": "line"
+        },
+        {
+          "value": "<font color=\"#F4000000\" size=\"14\" weight=\"normal\">${jam}</font>",
+          "key": "Jam",
+          "type": "data",
+          "background": "#00FFFFFF"
+        }
+      ]
+    }
+  ],
+  "receiptData": [
+    {
+      "value": "<font color=\"#F4000000\" size=\"21\" weight=\"bold\">${namaNasabah}</font>",
+      "key": "Nama Nasabah",
+      "type": "data",
+      "background": "#FFFDEB"
+    },
+    {
+      "value": "",
+      "key": "",
+      "type": "line"
+    },
+    {
+      "value": "<font color=\"#9E000000\" size=\"14\" weight=\"normal\">${tanggal}</font>",
+      "key": "Tanggal",
+      "type": "data",
+      "background": "#00FFFFFF"
+    },
+    {
+      "value": "<font color=\"#9E000000\" size=\"14\" weight=\"normal\">${jam}</font>",
+      "key": "Jam",
+      "type": "data",
+      "background": "#00FFFFFF"
+    },
+    {
+      "value": "<font color=\"#9E000000\" size=\"14\" weight=\"normal\">${email}</font>",
+      "key": "Alamat Email",
+      "type": "data",
+      "background": "#00FFFFFF"
+    },
+    {
+      "value": "<font color=\"#9E000000\" size=\"14\" weight=\"normal\">${nik}</font>",
+      "key": "NIK",
+'
+        ) || TO_CLOB(
+            '      "type": "data",
+      "background": "#00FFFFFF"
+    },
+    {
+      "value": "",
+      "key": "",
+      "type": "line"
+    },
+    {
+      "value": "<font color=\"#9E000000\" size=\"14\" weight=\"normal\">${riskProfile}</font>",
+      "key": "Profil Risiko",
+      "type": "data",
+      "background": "#00FFFFFF"
+    },
+    {
+      "value": "",
+      "key": "",
+      "type": "line"
+    },
+  ]
+}
+'
+        )
+    );
