cc3043f3c51ac1e7386249f501ae40336dd15d51
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-07-12 10:54:40 +0700

    add product category terproteksi

---
+++ 07_July/Tanggal_12/7_Insert_Business_Parameter_Product_Category_Terproteksi.sql
@@ -0,0 +1,34 @@
+INSERT INTO PARAMETER_MANAGEMENT_SERVICE.BUSINESS_PARAMETER (
+        CREATED_BY,
+        CREATED_DATE,
+        DESCRIPTION,
+        MODIFIED_BY,
+        MODIFIED_DATE,
+        MODULE,
+        NAME,
+        "ORDERING",
+        LABEL1,
+        LABEL2,
+        IMAGE_URL,
+        ADDITIONAL_DATA,
+        ISDELETED,
+        STATUS,
+        VALUE
+    )
+VALUES (
+        'SYSTEM',
+        TIMESTAMP '2023-07-12 10:52:00',
+        'Product Catalog Category Terproteksi',
+        NULL,
+        NULL,
+        'REKSADANA_CATEGORY',
+        'Terproteksi',
+        6,
+        NULL,
+        NULL,
+        'http://mf8.myinfosys.net:7005/bsi/assets/ReksaDana/ProductCatalogCategory/Terproteksi/terproteksi_2x.png',
+        NULL,
+        0,
+        'Aktif',
+        TO_CLOB('TERPROTEKSI')
+    );
