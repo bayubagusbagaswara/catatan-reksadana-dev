32cfe93f366bf01edcdafb9ff2a50c8599d660b3
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-07-12 10:45:59 +0700

    add product category pendapatan tetap

---
+++ 07_July/Tanggal_12/3_Insert_Business_Parameter_Product_Category_Pendapatan_Tetap.sql
@@ -0,0 +1,34 @@
+INSERT INTO PARAMETER_MANAGEMENT_SERVICE.BUSINESS_PARAMETER (
+        CREATED_BY,
+        CREATED_DATE,
+        DESCRIPTION,
+        MODIFIED_BY,
+        MODIFIED_DATE,
+        MODULE,
+        NAME,
+        "ORDERING",
+        LABEL1,
+        LABEL2,
+        IMAGE_URL,
+        ADDITIONAL_DATA,
+        ISDELETED,
+        STATUS,
+        VALUE
+    )
+VALUES (
+        'SYSTEM',
+        TIMESTAMP '2023-07-12 10:44:00',
+        'Product Catalog Category Pendapatan Tetap',
+        NULL,
+        NULL,
+        'REKSADANA_CATEGORY',
+        'Pendapatan Tetap',
+        2,
+        NULL,
+        NULL,
+        'http://mf8.myinfosys.net:7005/bsi/assets/ReksaDana/ProductCatalogCategory/PendapatanTetap/pendapatan_tetap_2x.png',
+        NULL,
+        0,
+        'Aktif',
+        TO_CLOB('PENDAPATAN_TETAP')
+    );
