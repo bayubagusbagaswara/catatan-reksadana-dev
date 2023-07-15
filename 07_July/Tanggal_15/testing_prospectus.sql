94239c8c6dfed697a22e1f5a84b2382a747907bc
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-07-15 10:03:41 +0700

    update

---
+++ 07_July/Tanggal_15/testing_prospectus.sql
@@ -0,0 +1,36 @@
+INSERT INTO PARAMETER_MANAGEMENT_SERVICE.CONTENT_PARAMETER (
+        CREATED_BY,
+        CREATED_DATE,
+        DESCRIPTION,
+        MODIFIED_BY,
+        MODIFIED_DATE,
+        MODULE,
+        NAME,
+        VALUE,
+        "LANGUAGE",
+        "ORDERING",
+        LABEL1,
+        LABEL2,
+        IMAGE_URL,
+        ADDITIONAL_DATA,
+        FLAG,
+        IS_DELETED
+    )
+VALUES (
+        'SYSTEM',
+        TIMESTAMP '2023-07-12 09:02:00',
+        'Prospectus MPUS (Base64 PDF)',
+        NULL,
+        NULL,
+        'REKSADANA_PROSPECTUS',
+        'MPUS',
+        '-',
+        'IDN',
+        1,
+        NULL,
+        NULL,
+        NULL,
+        TO_CLOB('TESTING'),
+        'BASE64',
+        0
+    );
