61d75498336b8908c1bf1954e678b96d4027234a
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-07-15 10:59:43 +0700

    add prospectus MISB

---
+++ 07_July/Tanggal_15/8_Insert_Prospectus_MISB.sql
@@ -0,0 +1,36 @@
+INSERT INTO PARAMETER_MANAGEMENT_SERVICE.CONTENT_PARAMETER (
+        CREATED_BY,
+        CREATED_DATE,
+        DESCRIPTION,
+        MODIFIED_BY,
+        MODIFIED_DATE,
+        MODULE,
+        NAME,
+        VALUE,
+        "LANGUAGE",
+        "ORDERING",
+        LABEL1,
+        LABEL2,
+        IMAGE_URL,
+        ADDITIONAL_DATA,
+        FLAG,
+        IS_DELETED
+    )
+VALUES (
+        'SYSTEM',
+        TIMESTAMP '2023-07-15 10:35:00',
+        'Prospectus MISB (Base64 PDF)',
+        NULL,
+        NULL,
+        'REKSADANA_PROSPECTUS',
+        'MISB',
+        '-',
+        'IDN',
+        1,
+        'Juni 2023',
+        NULL,
+        NULL,
+        TO_CLOB('TESTING'),
+        'BASE64',
+        0
+    );
