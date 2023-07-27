ecdf7b3d972fb6fdde3f3d7399c9b8bc2fa1ae5e
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-07-27 14:52:47 +0700

    insert content parameter risk profile conservative

---
+++ 07_July/Tanggal_27/1_Insert_Content_Parameter_Risk_Profile_Conservative.sql
@@ -0,0 +1,36 @@
+INSERT INTO PARAMETER_MANAGEMENT_SERVICE.CONTENT_PARAMETER (
+        CREATED_BY,
+        CREATED_DATE,
+        DESCRIPTION,
+        MODIFIED_BY,
+        MODIFIED_DATE,
+        MODULE,
+        NAME,
+        VALUE,
+        "LANGUAGE",
+        "ORDERING",
+        LABEL1,
+        LABEL2,
+        IMAGE_URL,
+        ADDITIONAL_DATA,
+        FLAG,
+        IS_DELETED
+    )
+VALUES (
+        'SYSTEM',
+        TIMESTAMP '2023-07-12 09:02:00',
+        'Reksa Dana Risk Profle Conservative',
+        NULL,
+        NULL,
+        'REKSADANA_RISK_PROFILE',
+        'CONSERVATIVE',
+        '-',
+        'IDN',
+        1,
+        NULL,
+        NULL,
+        NULL,
+        TO_CLOB('Tipe conservative adalah tipe investor dengan profil risiko paling rendah. Ciri-cirinya, investor tipe ini menginginkan investasi yang aman, tingkat imbal hasil (return) cenderung stabil, dan takut kalau investasi pokok berkurang'),
+        'UNKNOWN',
+        0
+    );
